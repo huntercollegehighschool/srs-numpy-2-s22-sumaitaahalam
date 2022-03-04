e61c6938a83e4209c9674ad82561dbdde3309eb1
sumaitaahalam <75225835+sumaitaahalam@users.noreply.github.com>
2022-03-04 17:35:34 +0000

    .

--- AColumnCypher.py
+++ AColumnCypher.py
@@ -18,6 +18,7 @@ In this exercise we will use numpy multidimensional arrays to encode a message a
 """
 1. Import numpy (as np) so that you have access to all numpy methods. Code: import numpy as np
 """
+import numpy as np
 
 
 """
@@ -26,20 +27,30 @@ In this exercise we will use numpy multidimensional arrays to encode a message a
 """
 message = "THISMESSAGEISVERYSECRETX"
 
+# goal: list with the characters in message
+message_lst = []
+for i in range(len(message)):
+  message_lst.append(message[i])
+
+#print(message)
+
+ames = np.array(message_lst)
 
 
 """
 3. Use <array>.shape = (<number of rows>, <number of columns>) to create a column cypher with 3 columns. Print the result
 Note: You can calculate how many rows you will need, or the shape function can do that automatically if you make the number of rows -1.
 """
-
+ames.shape = (3,-1)
 
 """
 4. Use <var> = np.transpose(<array>) to switch the rows and the columns. Print the new array.
 """
-
+swtch= np.transpose(ames)
+print(swtch)
 """
 5. When you index the transposed array, you should get a row of the array. For example, <array>[0] should be the first row of characters.
+
 
 Declare a new variable that will store the encoded message, and set it to an empty string. Loop through the rows of the array. Use ''.join(<array>) to convert the array of characters to a string. Then add that string to the encoded message. Print the final result.
 """
--- BPercentDailyReturn.py
+++ BPercentDailyReturn.py
@@ -7,5 +7,6 @@ Note: the percent daily return = 100 * (current day - previous day)/previous day
 
 Below you are given the stock prices (there are only 4 days in this example). First convert to a numpy array. Then use slicing and numeric operations to calculate the percent daily return (no for loops!).
 """
-
+import numpy as np
 acme = [10, 11.5, 11, 10, 12]
+ary= np.array(acme)
